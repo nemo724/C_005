d1099d7b6d0aa4adb9d3a19c12346ec8b3e34ad5
nemo <nemo724@naver.com>
2024-11-27 13:05:42 +0900

    난수 생성 함수 srand 추가

--- 005번/a005_playingCards.c
+++ 005번/a005_playingCards.c
@@ -29,7 +29,8 @@ int main(void)
 {int check_table[CARDS + 1];//1~52에 해당하는 각 카드가 생성되었는지 기록하는 테이블(1은 생성,0은 미생성)
 int shuffle_result[CARDS];//랜덤으로 섞은 카드의 순서를 기록하는 테이블
 char suit = 0;//카드의 문양 이니셜을 저장하는 변수
-
+ 
+srand((unsigned int)time(NULL));
 
 
 
