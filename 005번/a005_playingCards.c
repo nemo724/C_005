211ee1302b180800a8e3e424ec3e440988a6feda
nemo <nemo724@naver.com>
2024-11-27 13:14:44 +0900

    카드 값을 생성하는 함수 make_card 추가

--- 005번/a005_playingCards.c
+++ 005번/a005_playingCards.c
@@ -11,9 +11,16 @@
 int rand_max = 52;//생성될 수 있는 최대값
 int rand_min = 1;//생성될 수 있는 최솟값
 
+void make_card(int[],int[]);
 
-
-
+void make_card(int check_table[], int shuffle_result[])
+{for(int i =0;i<CARDS;i++)
+	{int index=0;
+	do { shuffle_result[i] = rand() % (rand_max - rand_min + 1) + rand_min; index=shuffle_result[i];}
+	while(check_table[index]==1);
+	check_table[index] = 1;
+	} 
+}
 
 
 
@@ -31,6 +38,8 @@ int shuffle_result[CARDS];//랜덤으로 섞은 카드의 순서를 기록하는
 char suit = 0;//카드의 문양 이니셜을 저장하는 변수
  
 srand((unsigned int)time(NULL));
+make_card(check_table, shuffle_result);
+
 
 
 
