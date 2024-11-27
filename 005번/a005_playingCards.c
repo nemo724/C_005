16c2fd1f4505e7dde4c7a21909b885e0c2ce4b9f
nemo <nemo724@naver.com>
2024-11-27 13:03:09 +0900

    rand_max,min 설명 주석 및 카드값에 따른 카드의 문약의 이니셜을 담는 변수 suit 추가

--- 005번/a005_playingCards.c
+++ 005번/a005_playingCards.c
@@ -8,8 +8,8 @@
 #define DIAMOND 3
 #define SCALE 13
 
-int rand_max = 52;
-int rand_min = 1;
+int rand_max = 52;//생성될 수 있는 최대값
+int rand_min = 1;//생성될 수 있는 최솟값
 
 
 
@@ -28,13 +28,13 @@ int rand_min = 1;
 int main(void)
 {int check_table[CARDS + 1];//1~52에 해당하는 각 카드가 생성되었는지 기록하는 테이블(1은 생성,0은 미생성)
 int shuffle_result[CARDS];//랜덤으로 섞은 카드의 순서를 기록하는 테이블
+char suit = 0;//카드의 문양 이니셜을 저장하는 변수
 
 
 
 
 
-
-	return 0;
+return 0;
 }
 
 
