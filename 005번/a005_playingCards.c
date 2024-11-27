65369315346fe08213a5911ee224694a80fa2034
nemo <nemo724@naver.com>
2024-11-27 12:53:39 +0900

    초기 커밋.

---
+++ 005번/a005_playingCards.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define CARDS 52
+#define CLOVER 0
+#define HEART 1
+#define SPADE 2
+#define DIAMOND 3
+#define SCALE 13
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+
+
+
+
+
+
+	return 0;
+}
+
+
